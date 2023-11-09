4728edbd871bcb803b6f7b79eb05117e39edf3a2
karoldziadkowiec <karol.dziadkkowiec@gmail.com>
2023-11-09 18:07:49 +0100

    09.11

--- Harry Potter Game/GameWindow.Designer.cs
+++ Harry Potter Game/GameWindow.Designer.cs
@@ -28,85 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameWindow));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.grass = new System.Windows.Forms.PictureBox();
+            this.building = new System.Windows.Forms.PictureBox();
+            this.cloud = new System.Windows.Forms.PictureBox();
+            this.harryPotter = new System.Windows.Forms.PictureBox();
+            this.scoreDESCLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.bestLabel = new System.Windows.Forms.Label();
+            this.bestDESCLabel = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.grass)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.building)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cloud)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.harryPotter)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // grass
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-2, 560);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1086, 95);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.grass.Image = ((System.Drawing.Image)(resources.GetObject("grass.Image")));
+            this.grass.Location = new System.Drawing.Point(-2, 560);
+            this.grass.Name = "grass";
+            this.grass.Size = new System.Drawing.Size(1086, 95);
+            this.grass.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.grass.TabIndex = 0;
+            this.grass.TabStop = false;
             // 
-            // pictureBox2
+            // building
             // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(651, 308);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(85, 253);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.building.Image = ((System.Drawing.Image)(resources.GetObject("building.Image")));
+            this.building.Location = new System.Drawing.Point(651, 308);
+            this.building.Name = "building";
+            this.building.Size = new System.Drawing.Size(85, 253);
+            this.building.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.building.TabIndex = 1;
+            this.building.TabStop = false;
             // 
-            // pictureBox3
+            // cloud
             // 
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(611, 0);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(162, 111);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
+            this.cloud.Image = ((System.Drawing.Image)(resources.GetObject("cloud.Image")));
+            this.cloud.Location = new System.Drawing.Point(611, 0);
+            this.cloud.Name = "cloud";
+            this.cloud.Size = new System.Drawing.Size(162, 111);
+            this.cloud.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cloud.TabIndex = 2;
+            this.cloud.TabStop = false;
             // 
-            // pictureBox4
+            // harryPotter
             // 
-            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(201, 183);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(145, 109);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox4.TabIndex = 3;
-            this.pictureBox4.TabStop = false;
+            this.harryPotter.BackColor = System.Drawing.Color.Transparent;
+            this.harryPotter.Image = ((System.Drawing.Image)(resources.GetObject("harryPotter.Image")));
+            this.harryPotter.Location = new System.Drawing.Point(175, 183);
+            this.harryPotter.Name = "harryPotter";
+            this.harryPotter.Size = new System.Drawing.Size(145, 109);
+            this.harryPotter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.harryPotter.TabIndex = 3;
+            this.harryPotter.TabStop = false;
+            // 
+            // scoreDESCLabel
+            // 
+            this.scoreDESCLabel.BackColor = System.Drawing.Color.Firebrick;
+            this.scoreDESCLabel.Font = new System.Drawing.Font("Algerian", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreDESCLabel.ForeColor = System.Drawing.Color.White;
+            this.scoreDESCLabel.Location = new System.Drawing.Point(416, 587);
+            this.scoreDESCLabel.Name = "scoreDESCLabel";
+            this.scoreDESCLabel.Size = new System.Drawing.Size(168, 39);
+            this.scoreDESCLabel.TabIndex = 4;
+            this.scoreDESCLabel.Text = "SCORE:";
+            this.scoreDESCLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.BackColor = System.Drawing.Color.Firebrick;
+            this.scoreLabel.Font = new System.Drawing.Font("Algerian", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.ForeColor = System.Drawing.Color.White;
+            this.scoreLabel.Location = new System.Drawing.Point(580, 587);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(81, 39);
+            this.scoreLabel.TabIndex = 5;
+            this.scoreLabel.Text = "0";
+            this.scoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // bestLabel
+            // 
+            this.bestLabel.BackColor = System.Drawing.Color.Firebrick;
+            this.bestLabel.Font = new System.Drawing.Font("Algerian", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bestLabel.ForeColor = System.Drawing.Color.Gold;
+            this.bestLabel.Location = new System.Drawing.Point(964, 587);
+            this.bestLabel.Name = "bestLabel";
+            this.bestLabel.Size = new System.Drawing.Size(81, 39);
+            this.bestLabel.TabIndex = 7;
+            this.bestLabel.Text = "0";
+            this.bestLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // bestDESCLabel
+            // 
+            this.bestDESCLabel.BackColor = System.Drawing.Color.Firebrick;
+            this.bestDESCLabel.Font = new System.Drawing.Font("Algerian", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bestDESCLabel.ForeColor = System.Drawing.Color.Gold;
+            this.bestDESCLabel.Location = new System.Drawing.Point(828, 587);
+            this.bestDESCLabel.Name = "bestDESCLabel";
+            this.bestDESCLabel.Size = new System.Drawing.Size(140, 39);
+            this.bestDESCLabel.TabIndex = 6;
+            this.bestDESCLabel.Text = "BEST:";
+            this.bestDESCLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
             // 
             // GameWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.ClientSize = new System.Drawing.Size(1082, 653);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.bestLabel);
+            this.Controls.Add(this.bestDESCLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.scoreDESCLabel);
+            this.Controls.Add(this.harryPotter);
+            this.Controls.Add(this.cloud);
+            this.Controls.Add(this.building);
+            this.Controls.Add(this.grass);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "GameWindow";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Harry Potter Game";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gameKeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gameKeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.grass)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.building)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cloud)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.harryPotter)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox grass;
+        private System.Windows.Forms.PictureBox building;
+        private System.Windows.Forms.PictureBox cloud;
+        private System.Windows.Forms.PictureBox harryPotter;
+        private System.Windows.Forms.Label scoreDESCLabel;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label bestLabel;
+        private System.Windows.Forms.Label bestDESCLabel;
+        private System.Windows.Forms.Timer gameTimer;
     }
 }
--- Harry Potter Game/GameWindow.cs
+++ Harry Potter Game/GameWindow.cs
@@ -12,9 +12,82 @@ namespace Harry_Potter_Game
 {
     public partial class GameWindow : Form
     {
+        int buildingSpeed = 8;
+        int cloudSpeed = 8;
+        int gravity = 10 ;
+        int score = 0;
+        int bestScore = 0;
+
         public GameWindow()
         {
             InitializeComponent();
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            harryPotter.Top += gravity;
+            cloud.Left -= cloudSpeed;
+            building.Left -= buildingSpeed;
+            scoreLabel.Text = score.ToString();
+
+            isTimeToEndGame();
+            saveScore();
+            checkTheBestScore();
+
+        }
+
+        private void gameKeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -6;
+            }
+        }
+
+        private void gameKeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 6 ;
+            }
+        }
+
+        private void isTimeToEndGame()
+        {
+            if (harryPotter.Bounds.IntersectsWith(cloud.Bounds) || harryPotter.Bounds.IntersectsWith(building.Bounds) || harryPotter.Bounds.IntersectsWith(grass.Bounds) || harryPotter.Top < -10)
+            {
+                gameTimer.Stop();
+            }
+        }
+
+        private void saveScore()
+        {
+            if (cloud.Left < -50)
+            {
+                cloud.Left = 830;
+                score++;
+            }
+            else if (building.Left < -100)
+            {
+                building.Left = 900;
+                score++;
+                increaseObstaclesSpeed();
+            }
+        }
+
+        private void increaseObstaclesSpeed()
+        {
+            buildingSpeed++;
+            cloudSpeed++;
+        }
+
+        private void checkTheBestScore()
+        {
+            if (score > bestScore) 
+            {
+                bestScore = score;
+                bestLabel.Text = bestScore.ToString();
+            }
         }
     }
 }
